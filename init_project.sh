aa08fe5773735039714f19d1558f3eea7004f183
plutonio <qwetyqwery@protonmail.com>
2021-09-16 11:45:51 +0300

    update init script

--- init_project.sh
+++ init_project.sh
@@ -6,6 +6,5 @@ docker-compose build --no-cache
 docker-compose up -d
 
 docker exec go-books make
-docker exec go-books goose -dir internal/migration mysql "books_user:books_pass@tcp(mysql-books:3306)/books_db?parseTime=true"  up
-docker exec go-books charlatan load ./tests/fixtures -u=books_user -d=books_db -p=books_pass --host=mysql-books
+docker exec go-books make db_init
 docker exec go-books make exec
